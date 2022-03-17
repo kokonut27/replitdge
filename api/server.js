c55e78d3378fa6f191900e56016757434a065191
kokomi simp :3 <beol0127@gmail.com>
2022-03-17 18:51:46 +0000

    chore: update files

---
+++ api/server.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let app = express();
+let fs = require("fs");
+
+app.get('/', (req, res) => {
+  res.redirect('https://github.com/kokonut27/replitdge');
+});
+
+app.get('/api', (req, res) => {
+  fs.readFile("themes.json", 'utf8', function (err, data) {
+    res.end( data );
+  });
+})
+
+app.listen(8080, function () {
+  console.log('replitdge is now running :D');
+})
