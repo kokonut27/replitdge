15b2e9a55320f6a92ff4ad7bb889f9854233e92e
kokomi simp :3 <beol0127@gmail.com>
2022-03-17 15:39:50 +0000

    chore: update routes

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ let express = require('express');
 let app = express();
 let fs = require("fs");
 
+app.get('/', function (req, res) {
+  res.redirect('https://github.com/kokonut27/replitdge')
+})
+
+app.get('/api', function (req, res) {
+  fs.readFile("themes.json", 'utf8', function (err, data) {
+    res.end( data );
+  });
+})
+
+let server = app.listen(8080, function () {
+  console.log('replitdge is now running :D');
+})
