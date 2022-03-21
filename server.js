ac6a9e77bd84443b0081c5976580c6d08cd134c6
kokomi simp :3 <beol0127@gmail.com>
2022-03-21 21:23:01 +0000

    chore: update whole api

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-let express = require('express');
-let app = express();
-let fs = require("fs");
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const https = require('https');
+
+
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', (req, res) => {
@@ -10,14 +14,32 @@ app.get('/', (req, res) => {
 app.get('/api', (req, res) => {
   let username = req.query.username;
   let theme = req.query.theme;
+  let url = `https://replit.com/@${username}`
+  const options = {
+    hostname: url,
+    method: 'GET'
+  }
+
+  const req2 = https.request(options, res2 => {
+    if (res2.statusCode != 404) {
+      console.log(`${username} username exists!`);
+    };
+  });
+
+  req2.on('error', error => {
+    res.send('<h1>Something went wrong! Try again later!</h1>');
+  });
   
   fs.readFile("themes.json", 'utf8', (err, data) => {
-    if (theme.includes(theme)) {
+    let themes = JSON.parse( data );
+    if (themes[theme]) {
       console.log(`${theme} theme exists!`)
     } else {
       res.send('<h1>Something went wrong! Try again later!</h1>');
     }
   });
+
+  
 })
 
 app.listen(8080, () => {
