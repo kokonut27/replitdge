bcdb3a1465975247bee8e91bf2a854e78aaddbaa
kokomi simp :3 <beol0127@gmail.com>
2022-03-21 17:56:13 +0000

    chore: update api route

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ let express = require('express');
 let app = express();
 let fs = require("fs");
 
-// https://www.tutorialspoint.com/nodejs/nodejs_restful_api.htm
 
 app.get('/', (req, res) => {
   res.redirect('https://github.com/kokonut27/replitdge');
 });
 
 app.get('/api', (req, res) => {
+  let username = req.query.username;
+  let theme = req.query.theme;
+  
   fs.readFile("themes.json", 'utf8', (err, data) => {
-    res.end(data);
+    if (theme.includes(theme)) {
+      console.log(`${theme} theme exists!`)
+    } else {
+      res.send('<h1>Something went wrong! Try again later!</h1>');
+    }
   });
 })
 
